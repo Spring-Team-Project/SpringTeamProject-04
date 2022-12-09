50c8d18ccb259e251eea2468724eb2be7e1ef770
nokme76 <111405361+nokme76@users.noreply.github.com>
2022-12-09 18:06:24 +0900

    테스팅 종료

--- src/main/java/com/bitc/springteamproject1209/controller/TestController.java
+++ src/main/java/com/bitc/springteamproject1209/controller/TestController.java
@@ -1,15 +1,35 @@
 package com.bitc.springteamproject1209.controller;
 
+import com.bitc.springteamproject1209.dto.TestDto;
+import com.bitc.springteamproject1209.service.TestService;
+import com.bitc.springteamproject1209.service.TestServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class TestController {
+
+  @Autowired
+  private TestService testService;
 
   @RequestMapping("/")
   public String index() throws Exception {
     return "index";
+  }
+
+
+  @RequestMapping("/testdb")
+  public ModelAndView openDB() throws Exception{
+    ModelAndView mv = new ModelAndView("testdb");
+
+    List<TestDto> testDtoList = testService.selectDBdto();
+    mv.addObject("testDtoList",testDtoList);
+
+    return mv;
   }
 
 }
--- src/main/java/com/bitc/springteamproject1209/service/TestServiceImpl.java
+++ src/main/java/com/bitc/springteamproject1209/service/TestServiceImpl.java
@@ -1,6 +1,20 @@
 package com.bitc.springteamproject1209.service;
 
-public class TestServiceImpl implements TestService{
+import com.bitc.springteamproject1209.dto.TestDto;
+import com.bitc.springteamproject1209.mapper.TestMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class TestServiceImpl implements TestService {
 
 
+    @Autowired
+    private TestMapper testMapper;
+
+    @Override
+    public List<TestDto> selectDBdto() throws Exception {
+        return testMapper.selectDBdto();
+    }
 }
